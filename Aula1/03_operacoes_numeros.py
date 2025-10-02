9d834a8a0bd16d3843d21a448fe74a5e4192b5ec
ricardo-as1 <ricardo@autobookr.com>
2025-10-02 01:39:20 -0300

    Refatoramento 3: Atualização do README.md, adição da atividade 03_operacoes_numeros.py e reorganização do arquivo 02_celsius_para_fahrenheit.py para o diretório Aula1.

---
+++ Aula1/03_operacoes_numeros.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Digite o primeiro número inteiro: "))
+numero2 = int(input("Digite o segundo número inteiro: "))
+numeroR = float(input("Digite um número real: "))
+produto = (numero1 * 2) * (numero2 / 2)
+soma = (numero1 * 3) + numeroR
+cubo = numeroR ** 3
+print("O produto do dobro do primeiro com metade do segundo é: ", produto)
+print("A soma do triplo do primeiro com o terceiro é: ", soma)
+print("O terceiro elevado ao cubo é: ", cubo)

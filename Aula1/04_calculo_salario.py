9a358590ccfe2ed5c704f95f3d7d2bb1f443cc63
ricardo-as1 <ricardo@autobookr.com>
2025-10-02 01:40:45 -0300

    Refatoramento 4: Atualização do README.md e adição da atividade 04_calculo_salario.py.

---
+++ Aula1/04_calculo_salario.py
@@ -0,0 +1,4 @@
+valorhora = float(input("Quanto você ganha por hora? "))
+horastrabalhadas = int(input("Quantas horas você trabalha no mês? "))
+salario = valorhora * horastrabalhadas
+print("O seu salário no mês é: R$", salario)

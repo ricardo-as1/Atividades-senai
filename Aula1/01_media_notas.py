32818d0f848273f65675d8781d7059f16a9fd876
ricardo-as1 <ricardo@autobookr.com>
2025-10-02 01:35:35 -0300

    Refatoramento 1: Remoção de arquivos C++ e Python não utilizados, mantendo apenas o script relevante para a aula 1.

---
+++ Aula1/01_media_notas.py
@@ -0,0 +1,6 @@
+nota1 = int(input("Nota do primeiro bimestre: "))
+nota2 = int(input("Nota do segundo bimestre: "))
+nota3 = int(input("Nota do terceiro bimestre: "))
+nota4 = int(input("Nota do quarto bimestre: "))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print("A média do aluno no ano é: ", media)

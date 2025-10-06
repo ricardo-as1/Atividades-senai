169eb62e78df6733e4a480cb8f58542ee0a4a789
ricardo-as1 <ricardo@autobookr.com>
2025-10-06 10:34:29 -0300

    Aula - IMC: Calcula o Índice de Massa Corporal (IMC) e classifique a pessoa conforme o IMC.

---
+++ Aula2/08_IMC.py
@@ -0,0 +1,26 @@
+# Entrada de dados
+peso = float(input("Informe o peso em kg: "))
+altura = float(input("Informe a altura em metros: "))
+
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif 18.5 <= imc < 25:
+        return "Peso normal"
+    elif 25 <= imc < 30:
+        return "Sobrepeso"
+    elif 30 <= imc < 35:
+        return "Obesidade grau I"
+    elif 35 <= imc < 40:
+        return "Obesidade grau II"
+    else:
+        return "Obesidade grau III (mórbida)"
+
+imc = calcular_imc(peso, altura)
+classificacao = classificar_imc(imc)
+
+print(f"Seu IMC é: {imc:.2f}")
+print(f"Classificação: {classificacao}")
